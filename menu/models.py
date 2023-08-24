99bc4d29740b3e452fff9b53431ab2bfcaa26b02
Natalitta <nataliialebedieva@gmail.com>
2023-08-24 20:39:50 +0000

    Add models for menu items and menu display

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class MenuItem(models.Model):
+    # Model for menu items
+    title = models.CharField(max_length=30, unique=True)
+    description = models.TextField(default="")
+    featured_image = CloudinaryField('image', default='placeholder')
+    price = models.FloatField(default=0.00)
+    vegan = models.BooleanField(default=False)
+
+    class Meta:
+        # Order by name
+        ordering = ['title']
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Menu(models.Model):
+    # Model to create a menu
+    title = models.CharField(max_length=25, unique=True)
+    cakes = models.ManyToManyField('MenuItem', related_name='cakes')
+    pastry = models.ManyToManyField('MenuItem', related_name='pastry')
+
+    class Meta:
+        # Order by name
+        ordering = ['title']
+
+    def __str__(self):
+        return str(self.title)
