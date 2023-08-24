578a2d7fc488564873d1318d0ba472a67cf0c285
Natalitta <nataliialebedieva@gmail.com>
2023-08-24 21:39:53 +0000

    Add templates for menu display and base template

---
+++ menu/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("", views.MenuList.as_view(), name="home"),
+    # path('booking/', views.Order.as_view(), name='booking'),
+    # path('all_bookings/', views.BookingsList.as_view(), name='all_bookings'),
+    # path('edit/<slug:pk>/', views.EditBooking.as_view(), name='edit_booking'),
+    # path('delete/<slug:pk>/', views.DeleteBooking.as_view(), name='del_booking'),
+]
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+# from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import MenuItem
+# from .forms import BookingForm
+from datetime import timedelta, date
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
 
-# Create your views here.
+
+class MenuList(generic.ListView):
+    model = MenuItem
+    queryset = MenuItem.objects.order_by("title")
+    template_name = "index.html"
