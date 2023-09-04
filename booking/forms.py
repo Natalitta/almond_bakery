36a8fd745978d602f77da870a07bd5fd90c0a6aa
Natalitta <nataliialebedieva@gmail.com>
2023-09-04 22:02:40 +0000

    Update text in booking template

--- booking/forms.py
+++ booking/forms.py
@@ -1,5 +1,6 @@
 from .models import Booking
 from django import forms
+from django.contrib.auth.models import User
 
 
 class DateInput(forms.DateInput):
@@ -10,9 +11,10 @@ class BookingForm(forms.ModelForm):
 
     class Meta:
         model = Booking
-        fields = ('booked_item', 'customer', 'phone', 'number_of_items',
-                'personal_message', 'booking_date', 'delivery_time',
-                'home_delivery', 'address')
+        fields = (
+            'booked_item', 'customer', 'phone', 'number_of_items',
+            'personal_message', 'booking_date', 'delivery_time',
+            'home_delivery', 'address')
         labels = {
             'booked_item': 'Cake',
             'customer': 'Your Name',
