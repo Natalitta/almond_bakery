c6b0635258229398bb0f546cfaf929eacc13df7d
Natalitta <nataliialebedieva@gmail.com>
2023-09-04 19:58:05 +0000

    Fix editting issue

--- booking/views.py
+++ booking/views.py
@@ -6,6 +6,7 @@ from .forms import BookingForm
 from datetime import timedelta, date
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 
 
 class BookingCake(SuccessMessageMixin, LoginRequiredMixin, CreateView):
@@ -39,18 +40,29 @@ class BookingList(LoginRequiredMixin, ListView):
     template_name = 'all_bookings.html'
 
 
+class SuccessMessageMixin:
+    # Add a message if form submitted successfully.
+    success_message = ''
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        success_message = self.get_success_message(form.cleaned_data)
+        if success_message:
+            messages.success(self.request, success_message)
+        return response
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % cleaned_data
+
+
 class EditBooking(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     form_class = BookingForm
     template_name = 'edit_booking.html'
-    success_url = "all_bookings"
+    success_url = 'all_bookings'
     success_message = 'You have successfully updated your order.'
     model = Booking
 
     def form_valid(self, form):
-        messages.success(
-            request,
-            'You have successfully updated your order.'
-        )
         return super(EditBooking, self).form_valid(form)
 
     def test_func(self):
@@ -60,14 +72,10 @@ class EditBooking(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, 
 class DeleteBooking(SuccessMessageMixin, LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Booking
     template_name = 'confirm_delete.html'
-    success_url = "all_bookings"
+    success_url = 'all_bookings'
     success_message = 'You have successfully deleted your order.'
 
     def form_valid(self, form):
-        messages.success(
-            request,
-            'You have successfully deleted your order'
-        )
         return super(DeleteBooking, self).form_valid(form)
 
     def test_func(self):
