2e07a6010edb193ca400f9bf03529a81147d519f
Natalitta <nataliialebedieva@gmail.com>
2023-12-01 23:06:47 +0000

    Add testing

--- booking/tests.py
+++ booking/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from booking.models import Booking
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    
+    # Test booking app for logged in user
+    
+    def test_setUp(self):
+        # Setup test
+        username = "Emily"
+        password = "emily12345"
+        user_model = get_user_model()
+
+        # Create user
+        self.user = user_model.objects.create_user(
+            username=username,
+            password=password,
+            is_superuser=False
+        )
+        logged_in = self.client.login(username=username, password=password)
+        self.assertTrue(logged_in)
+
+        # Create booking
+        booking = Booking.objects.create(
+            customer=self.user,
+            booked_item=booked_item,
+            number_of_items=1,
+            booking_date=date.today(),
+            address=address,
+            )
--- menu/tests.py
+++ menu/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    
+    # Test Home page
+    
+    def test_home_page(self):
+        # Test correct home page is displayed
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 400)
+        self.assertTemplateUsed(response, 'home/index.html')
